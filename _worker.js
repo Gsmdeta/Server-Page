6399d33e38e9092ef18c8da3c31abb8e86ecbb69
GSM Deta <31906155+Gsmdeta@users.noreply.github.com>
2025-10-15 02:32:14 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,34 +1,9 @@
 export default {
-    async fetch(request, env) {
-        try {
-            const url = new URL(request.url);
-            url.hostname = env.HOST || 'ictfz.ir';
-            url.protocol = 'https:';
-
-            // Forward the modified request
-            const newRequest = new Request(url, {
-                method: request.method,
-                headers: request.headers,
-                body: request.body,
-                redirect: request.redirect,
-                credentials: request.credentials,
-            });
-
-            return await fetch(newRequest);
-        } catch (error) {
-            return new Response(`Error: ${error.message}`, { status: 500 });
-        }
-    }
-};
-
-
-/*
-export default {
-    async fetch(request, env) {
+       async fetch(request, env, ctx) {
                         const url = new URL(request.url);
-                        url.hostname = env.HOST || 'ictfz.ir';
+                        url.hostname = env.HOST || 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
-                        return fetch(request);      
-    }
-};*/
+                        return fetch(request);
+              }
+};
